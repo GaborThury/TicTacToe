1367e9dcaa74f2ad12300ecf4794602f4fce0dd6
Gabor Thury <thury.gabor1994@gmail.com>
2018-08-07 14:52:07 +0200

    made a few changes, edited info messages

--- src/TickTackToe.java
+++ src/TickTackToe.java
@@ -17,24 +17,30 @@ public class TickTackToe extends JFrame implements ActionListener{
         } else {
             this.size = size;
         }
+
         steps = 0;
         this.toWin = toWin;
         firstPlayer = true;
+        buttons = new JButton[this.size][this.size];
+
         setTitle("Tick Tack Toe");
         setSize(500, 500);
-        setBackground(Color.black);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new GridLayout(this.size, this.size));
-        buttons = new JButton[this.size][this.size];
+
         generateButtons();
     }
+
     private void generateButtons() {
+        Font font = new Font(Font.SANS_SERIF, Font.BOLD, 20);
+        Insets m = new Insets(0, 0, 0, 0);
+
         for (int i = 0; i < this.size; i++) {
             for (int j = 0; j < this.size; j++) {
                 JButton button = new JButton();
                 button.setText("");
-                button.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
-                button.setMargin(new Insets(0, 0, 0, 0));
+                button.setFont(font);
+                button.setMargin(m);
                 button.setBackground(Color.white);
                 button.setActionCommand(j + " " + i);
                 button.addActionListener(this);
@@ -43,6 +49,7 @@ public class TickTackToe extends JFrame implements ActionListener{
             }
         }
     }
+
     private boolean checkWinner (String player) {
         for (int i = 0; i < this.size; i++) {
             int sameNextToEachOther = 0;
@@ -84,8 +91,6 @@ public class TickTackToe extends JFrame implements ActionListener{
     }
 
 
-
-
     @Override
     public void actionPerformed(ActionEvent e) {
         // System.out.println(e.getActionCommand());
@@ -104,10 +109,10 @@ public class TickTackToe extends JFrame implements ActionListener{
             steps++;
         }
         if (checkWinner(buttons[i][j].getText())) {
-            JOptionPane.showMessageDialog(this, "Game is over! " + buttons[i][j].getText() + " have won!");
+            JOptionPane.showMessageDialog(this, "Game is over! " + buttons[i][j].getText() + " have won!", "Game over!", JOptionPane.INFORMATION_MESSAGE);
             clear();
         } else if (steps == this.size * this.size) {
-            JOptionPane.showMessageDialog(this, "Game is over! Draw!");
+            JOptionPane.showMessageDialog(this, "Game is over!\nDraw!", "Game over!", JOptionPane.INFORMATION_MESSAGE);
             clear();
         }
     }
